3ef2d4e380b186b4cf2de2abdc53827f888d7dcf
Mark Lagman <mark.lagman@paymaya.com>
2022-03-04 09:41:12 +0800

    Initial commit -- pipeline not included

---
+++ lib/hitcounter.d.ts
@@ -0,0 +1,12 @@
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
+import { Construct } from 'constructs';
+export interface HitCounterProps {
+    downstream: lambda.IFunction;
+    readCapacity: number;
+}
+export declare class HitCounter extends Construct {
+    readonly handler: lambda.Function;
+    readonly table: dynamodb.Table;
+    constructor(scope: Construct, id: string, props: HitCounterProps);
+}
